1471a56ec92a82fe3e3524de2b51a25d21641539
nahidmahmood35 <nahidmahmood35@gmail.com>
2024-06-01 11:44:16 +0600

    two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageOneComponent} from './page-one/page-one.component';
+import {PagetwoComponent} from './pagetwo/pagetwo.component';
+import { DeshboardComponent } from './deshboard/deshboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DeshboardComponent,
+  },
+  {
+    path: 'Dashboard',
+    component: DeshboardComponent,
+  },
+  {
+    path: 'one',
+    component: PageOneComponent,
+  },
+  {
+    path: 'two',
+    component: PagetwoComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
